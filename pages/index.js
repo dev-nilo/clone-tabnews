75f71063a593980839c458a4a1ecac873bbce561
Nilo Venturin <147455775+dev-nilo@users.noreply.github.com>
2024-12-04 21:10:26 +0000

    adiciona script `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <>
-            <h1>Boa noite, dog!!!</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Boa noite, dog!!!</h1>
+    </>
+  );
 }
 
 export default Home;
