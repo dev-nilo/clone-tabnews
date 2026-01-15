e0095311a5a50ff68d57c0d0ce1d92c4ed8fb3b6
Nilo Venturin <147455775+dev-nilo@users.noreply.github.com>
2026-01-15 01:39:10 +0000

    A message for me in the end of the year

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Testing CI/CD</h1>;
+  return (
+    <div>
+      <h1>Ok, I'm back!</h1>
+      <h2>
+        For me in the future, I want to be able to write code that is more
+        efficient and easier to read. I also want to learn more about different
+        programming languages and frameworks, so I can expand my skill set and
+        be more versatile as a developer.
+      </h2>
+      <p>
+        Remember to keep improving and learning new things! See you in
+        01/12/2026
+      </p>
+    </div>
+  );
 }
 
 export default Home;
